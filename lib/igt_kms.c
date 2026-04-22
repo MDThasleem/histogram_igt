mkahola/igt

commit ac37e1174cc49d689e82f35930a419851c7abb0f

lib/kms: Pimp the primary plane swapping
Do the primary plane swap a bit more cleanly with a pair of
igt_swap()s. And while at it sprinkle some asserts around
to make sure we didn't screw anything up.

Reviewed-by: Jani Nikula <jani.nikula@intel.com>
Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

lib/igt_kms.c
@@ -3025,7 +3025,6 @@ void igt_display_reset_outputs(igt_display_t *display)
 			igt_plane_t *old_primary = &pipe->planes[0];
 			igt_plane_t *new_primary =
 				igt_get_assigned_primary(output, pipe);
-			int new_primary_index = new_primary->index;
 
 			/*
 			 * If the driver-assigned primary plane isn't at
@@ -3034,12 +3033,21 @@ void igt_display_reset_outputs(igt_display_t *display)
 			 *
 			 * This way, the primary plane is always at index 0.
 			 */
-			if (new_primary_index != 0) {
-				new_primary->index = 0;
-				old_primary->index = new_primary_index;
+			if (new_primary->index != 0) {
+				igt_assert(old_primary != new_primary);
 
-				igt_swap(pipe->planes[new_primary_index],
-					 pipe->planes[0]);
+				igt_assert_eq(old_primary->index, 0);
+				igt_assert_neq(new_primary->index, 0);
+
+				igt_swap(*old_primary, *new_primary);
+				igt_swap(old_primary->index, new_primary->index);
+
+				igt_assert_neq(old_primary->index, 0);
+				igt_assert_eq(new_primary->index, 0);
+			} else {
+				igt_assert(old_primary == new_primary);
+
+				igt_assert_eq(old_primary->index, 0);
 			}
 		}
 	}

MDThasleem/histogram_igt

commit 4963e78e038806cc75f885d7d5713bb0d12c01d1

tests/kms: correct index validation logic
Based on the original implementation and the comment above the function,
it appears the expected behavior is for the plane indices to remain
consistent with those in crtc->planes. Therefore, after swapping the old
and new primary planes, we must update the index in igt_plane_t to
ensure it matches the corresponding index in crtc->planes.

Therefore, we should expect the indices of the old and new primary
planes to remain unchanged after swapping them.

Fixes: ac37e1174cc4 ("lib/kms: Pimp the primary plane swapping")
Signed-off-by: Jing-Ping Jan <jingpingjan@google.com>
Reviewed-by: Karthik B S <karthik.b.s@intel.com>

lib/igt_kms.c
@@ -2981,8 +2981,8 @@ void igt_display_reset_outputs(igt_display_t *display)
 				igt_swap(*old_primary, *new_primary);
 				igt_swap(old_primary->index, new_primary->index);
 
-				igt_assert_neq(old_primary->index, 0);
-				igt_assert_eq(new_primary->index, 0);
+				igt_assert_eq(old_primary->index, 0);
+				igt_assert_neq(new_primary->index, 0);
 			} else {
 				igt_assert(old_primary == new_primary);
 