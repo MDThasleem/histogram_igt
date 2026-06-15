9b33225c761bfe8c8c266bc56558d75c700029fb
Kamil Konieczny <kamil.konieczny@linux.intel.com>
2026-06-15 19:44:22 +0200

    include/linux_scaffold: drop _AC macro
    The macro _AC is causing a compilation error on current android
    using linux 7.0 uapi headers on RISCV. It seems the macro is not
    really needed as it is encoding only two constants, 0UL/0ULL and
    1UL/1ULL. Drop _AC macro and write out directly these numbers.
    
    Cc: enh <enh@google.com>
    Cc: "Zbigniew Kempczyński" <zbigniew.kempczynski@intel.com>
    Signed-off-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Reviewed-by: Zbigniew Kempczyński <zbigniew.kempczynski@intel.com>

--- include/linux_scaffold.h
+++ include/linux_scaffold.h
@@ -22,30 +22,14 @@ static inline s64 sign_extend64(u64 value, int index)
 	return (s64)(value << shift) >> shift;
 }
 
-#ifndef _AC
-#  define _AC(X, Y)      X##Y
-#else
-#  error "_AC macro already defined"
-#endif
-
 /* Make IGT build with Kernels < 4.17 */
-#ifndef _AC
-#  define _AC(X, Y)	__AC(X, Y)
-#endif
-#ifndef _UL
-#  define  _UL(x)		(_AC(x, UL))
-#endif
-#ifndef _ULL
-#  define _ULL(x)		(_AC(x, ULL))
-#endif
-
 #define GENMASK(h, l) \
-	(((~_UL(0)) - (_UL(1) << (l)) + 1) & \
-	(~_UL(0) >> (BITS_PER_LONG - 1 - (h))))
+	((~0UL - (1UL << (l)) + 1) & \
+	(~0UL >> (BITS_PER_LONG - 1 - (h))))
 
 #define GENMASK_ULL(h, l) \
-	(((~_ULL(0)) - (_ULL(1) << (l)) + 1) & \
-	(~_ULL(0) >> (BITS_PER_LONG_LONG - 1 - (h))))
+	((~0ULL - (1ULL << (l)) + 1) & \
+	(~0ULL >> (BITS_PER_LONG_LONG - 1 - (h))))
 
 #define BITS_PER_BYTE 8
 #define BITS_PER_TYPE(t) (sizeof(t) * BITS_PER_BYTE)
