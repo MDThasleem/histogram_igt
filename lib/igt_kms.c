psennats/drm.igt-gpu-tools

commit 3830ca6a5068712ad72498fb0c472fdbf42607fa

lib/igt_kms: Add support to check joiner mode limit
Added library changes to find the highest non-joiner mode.

v2: Update commit message, create a new library function
    to get max hdisplay and fix condtion for selecting mode.
v3: Update function name.

Signed-off-by: Jeevan B <jeevan.b@intel.com>
Reviewed-by: Karthik B S <karthik.b.s@intel.com>

lib/igt_kms.c
@@ -6407,6 +6407,55 @@ bool bigjoiner_mode_found(int drm_fd, drmModeConnector *connector,
 }
 
 /**
+ * get_max_hdisplay:
+ * @drm_fd: drm file descriptor
+ *
+ * Returns: The maximum hdisplay supported per pipe.
+ */
+static int get_max_pipe_hdisplay(int drm_fd)
+{
+	int dev_id = intel_get_drm_devid(drm_fd);
+
+	return (intel_display_ver(dev_id) >= 30) ? HDISPLAY_6K_PER_PIPE :
+						   HDISPLAY_5K_PER_PIPE;
+}
+
+/**
+ * max_non_joiner_mode_found:
+ * @drm_fd: drm file descriptor
+ * @connector: libdrm connector
+ * @max_dot_clock: max dot clock frequency
+ * @mode: libdrm mode to be filled
+ *
+ * Finds the highest possible display mode that does
+ * not require a big joiner.
+ *
+ * Returns: True if a valid non-joiner mode is found,
+ * false otherwise.
+ */
+bool max_non_joiner_mode_found(int drm_fd, drmModeConnector *connector,
+			   int max_dotclock, drmModeModeInfo *mode)
+{
+	int max_hdisplay = get_max_pipe_hdisplay(drm_fd);
+
+	for (int i = 0; i < connector->count_modes; i++) {
+		drmModeModeInfo *current_mode = &connector->modes[i];
+
+		if (current_mode->hdisplay == max_hdisplay &&
+		    current_mode->clock < max_dotclock) {
+			*mode = *current_mode;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/* TODO: Move these lib functions to the joiner-specific library file
+ *	 once those patches are merged.
+ */
+
+/**
  * igt_is_joiner_enabled_for_pipe:
  * @drmfd: A drm file descriptor
  * @pipe: display pipe

lib/igt_kms.h
@@ -1245,6 +1245,8 @@ int igt_get_max_dotclock(int fd);
 bool igt_bigjoiner_possible(int drm_fd, drmModeModeInfo *mode, int max_dotclock);
 bool bigjoiner_mode_found(int drm_fd, drmModeConnector *connector,
 			  int max_dotclock, drmModeModeInfo *mode);
+bool max_non_joiner_mode_found(int drm_fd, drmModeConnector *connector,
+			       int max_dotclock, drmModeModeInfo *mode);
 bool igt_is_joiner_enabled_for_pipe(int drmfd, enum pipe pipe);
 bool igt_ultrajoiner_possible(drmModeModeInfo *mode, int max_dotclock);
 bool ultrajoiner_mode_found(int drm_fd, drmModeConnector *connector,

MDThasleem/histogram_igt

commit d82df5f8af4ec773923140902d346a752e1f820e

lib/igt_kms: Rename max_non_joiner_mode_found to intel_boundary_non_joiner_mode_found
The function checks if a connector supports a mode at the platform's
maximum single-pipe hdisplay boundary. Rename to clarify intent with
intel_ prefix to indicate Intel-specific semantics.

Update all call sites in tests to use the new name.

Fixes: 3830ca6a5068 ("lib/igt_kms: Add support to check joiner mode limit")
Signed-off-by: Kunal Joshi <kunal1.joshi@intel.com>
Reviewed-by: Suraj Kandpal <suraj.kandpal@intel.com>

lib/igt_kms.c
@@ -7084,20 +7084,20 @@ bool bigjoiner_mode_found(int drm_fd, drmModeConnector *connector,
 }
 
 /**
- * max_non_joiner_mode_found:
+ * intel_boundary_non_joiner_mode_found:
  * @drm_fd: drm file descriptor
  * @connector: libdrm connector
  * @max_dot_clock: max dot clock frequency
  * @mode: libdrm mode to be filled
  *
- * Finds the highest possible display mode that does
- * not require a big joiner.
+ * Check if connector supports a mode at the platform's maximum single-pipe
+ * hdisplay boundary.
  *
- * Returns: True if a valid non-joiner mode is found,
+ * Returns: True if a valid non-joiner mode at the boundary is found,
  * false otherwise.
  */
-bool max_non_joiner_mode_found(int drm_fd, drmModeConnector *connector,
-			   int max_dotclock, drmModeModeInfo *mode)
+bool intel_boundary_non_joiner_mode_found(int drm_fd, drmModeConnector *connector,
+					  int max_dotclock, drmModeModeInfo *mode)
 {
 	int max_hdisplay = get_max_pipe_hdisplay(drm_fd);
 

lib/igt_kms.h
@@ -1344,8 +1344,8 @@ int igt_get_current_cdclk(int fd);
 bool igt_bigjoiner_possible(int drm_fd, drmModeModeInfo *mode, int max_dotclock);
 bool bigjoiner_mode_found(int drm_fd, drmModeConnector *connector,
 			  int max_dotclock, drmModeModeInfo *mode);
-bool max_non_joiner_mode_found(int drm_fd, drmModeConnector *connector,
-			       int max_dotclock, drmModeModeInfo *mode);
+bool intel_boundary_non_joiner_mode_found(int drm_fd, drmModeConnector *connector,
+					   int max_dotclock, drmModeModeInfo *mode);
 bool igt_is_joiner_enabled_for_pipe(int drmfd, enum pipe pipe);
 bool igt_ultrajoiner_possible(int drmfd, drmModeModeInfo *mode, int max_dotclock);
 bool ultrajoiner_mode_found(int drm_fd, drmModeConnector *connector,

tests/intel/kms_joiner.c
@@ -593,9 +593,9 @@ static void test_basic_max_non_joiner(data_t *data)
 				igt_assert_f(crtc, "There is no pipe %s\n", kmstest_pipe_name(pipe));
 
 				igt_output_set_crtc(output, crtc);
-				igt_require(max_non_joiner_mode_found(data->drm_fd,
-								      output->config.connector,
-								      max_dotclock, &mode));
+				igt_require(intel_boundary_non_joiner_mode_found(data->drm_fd,
+										  output->config.connector,
+										  max_dotclock, &mode));
 				igt_output_override_mode(output, &mode);
 				igt_info("Appplying mode = %dx%d@%d\n", mode.hdisplay,
 					 mode.vdisplay, mode.vrefresh);
@@ -658,8 +658,8 @@ int igt_main()
 			 */
 			bigjoiner_found = bigjoiner_mode_found(data.drm_fd, connector, max_dotclock, &mode);
 			ultrajoiner_found = ultrajoiner_mode_found(data.drm_fd, connector, max_dotclock, &mode);
-			non_joiner_found = max_non_joiner_mode_found(data.drm_fd, connector,
-								     max_dotclock, &mode);
+			non_joiner_found = intel_boundary_non_joiner_mode_found(data.drm_fd, connector,
+										 max_dotclock, &mode);
 
 			if (igt_has_force_joiner_debugfs(data.drm_fd, output->name))
 				force_joiner_supported = true;